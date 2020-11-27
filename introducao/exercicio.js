dcca70acc784c83bdb2c8586fa5cb616ab08d33f
Antônio Luis <antonioluisp97@gmail.com>
2020-11-26 22:05:55 -0300

    introducao

---
+++ introducao/exercicio.js
@@ -0,0 +1,4 @@
+
+function recebeObj(endereco) {
+    console.log('O usuário mora em ' + endereco.cidade + '/' + endereco.uf + ', no bairro ' + endereco.bairro + ', na rua "' + endereco.rua + '" com nº ' + endereco.numero)
+}
